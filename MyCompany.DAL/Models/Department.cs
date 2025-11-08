c047dd169482c30eff406f0c9223c88cb452bc88
Yahya Mahmoud <yahmahr@gmail.com>
2025-11-08 14:26:28 +0200

    DAL for Department table.

---
+++ MyCompany.DAL/Models/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCompany.DAL.Models
+{
+    internal class Department
+    {
+        public int DepartmentId { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfCreation { get; set; }
+    }
+}
